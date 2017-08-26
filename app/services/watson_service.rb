5ea2144df17faa965f658b9e9fb1e670d770be95
Carl Richmond <richmond.carl@outlook.com>
2017-08-25 23:40:25 -0600

    Trying to get tone_chat connection working with watson by creating json utterances object

--- app/services/watson_service.rb
+++ app/services/watson_service.rb
@@ -1,3 +1,4 @@
+require 'tempfile'
 class WatsonService
   attr_reader :text
 
@@ -13,6 +14,22 @@ class WatsonService
     response = conn.get("/tone-analyzer/api/v3/tone") do |request|
       request.params['Content-Type'] = "application/json"
       request.params['text'] = url_encode(@text)
+      request.params['version'] = "2017-08-25"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def analyze_tone_chat(tone_chat_input)
+    # File.open("tone_chat/temp.json","w") do |file|
+    #   file.write(JSON.pretty_generate(tone_chat_input))
+    # end
+    # conn.request :multipart
+    # conn.response :logger
+    # conn.adapter :net_http
+    conn.basic_auth("#{ENV['watson_tone_username']}", "#{ENV['watson_tone_password']}")
+    response = conn.get("/tone-analyzer/api/v3/tone_chat") do |request|
+      request.params['Content-Type'] = "application/json"
+      request.params['body'] = tone_chat_input.to_json
       request.params['version'] = "2017-08-25"
     end
     output = JSON.parse(response.body, symbolize_names: true)
--- spec/services/watson_service_object_spec.rb
+++ spec/services/watson_service_object_spec.rb
@@ -6,11 +6,37 @@ describe "watson object can return json information" do
     @text = "A string of words to send to Watson!!!!!#$%^"
     @watson = WatsonService.new(@text)
   end
+
   it "can be initialized and attributes called" do
     expect(@watson.text).to eq(@text)
   end
 
-  it "can communicate with IBM watson" do
-    expect(@watson.analyze_tone).to eq()
+  it "can communicate with IBM watson for tone analysis" do
+    json_response = @watson.analyze_tone
+    expect(json_response[:document_tone]).to eq(json_response[:document_tone])
+  end
+
+  xit "can communicate with IBM watson for tone chat analysis" do
+    tone_chat_input = {
+      "utterances": [
+        {
+          "text": "Hello, I'm having a problem with your product.",
+          "user": "customer"
+        },
+        {
+          "text": "OK, let me know what's going on, please.",
+          "user": "agent"
+        },
+        {
+          "text": "Well, nothing is working :(",
+          "user": "customer"
+        },
+        {
+          "text": "Sorry to hear that.",
+          "user": "agent"
+        }
+      ]
+    }
+    json_response = @watson.analyze_tone_chat(tone_chat_input)
   end
 end
