852c4a90415fad7b468f1b16dd811f37926c1751
Carl Richmond <richmond.carl@outlook.com>
2017-08-25 21:51:51 -0600

    Added Figaro. Created url encoder method. Created connection with IBM watson user service credentials.

--- app/services/watson_service.rb
+++ app/services/watson_service.rb
@@ -4,9 +4,27 @@ class WatsonService
   def initialize(user = nil, string)
     @user = user
     @text = string
+    @conn = Faraday.new(:url => 'https://gateway.watsonplatform.net')
+  end
+
+  def analyze_tone
+    url_string = url_encode(text)
+    conn.basic_auth("#{ENV['watson_tone_username']}", "#{ENV['watson_tone_password']}")
+    response = conn.get("/tone-analyzer/api/v3/tone") do |request|
+      request.params['Content-Type'] = "application/json"
+      request.params['text'] = url_encode(@text)
+      request.params['version'] = "2017-08-25"
+    end
+    output = JSON.parse(response.body, symbolize_names: true)
   end
 
 
   private
+
+  attr_reader :conn
+
+  def url_encode(string)
+    ERB::Util.url_encode(string)
+  end
 
 end
--- spec/services/watson_service_object_spec.rb
+++ spec/services/watson_service_object_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 describe "watson object can return json information" do
+
+  before(:each) do
+    @text = "A string of words to send to Watson!!!!!#$%^"
+    @watson = WatsonService.new(@text)
+  end
   it "can be initialized and attributes called" do
-    text = "A string of words to send to Watson"
-    watson = WatsonService.new(text)
-    expect(watson.text).to eq(text)
+    expect(@watson.text).to eq(@text)
+  end
+
+  it "can communicate with IBM watson" do
+    expect(@watson.analyze_tone).to eq()
   end
 end
