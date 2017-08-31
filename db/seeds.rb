48217c911cc1ab3507e052c118270f174d948ceb
Carl Richmond <richmond.carl@outlook.com>
2017-08-31 11:12:26 -0600

    Created validators for the tone_chat IBM resonse. Edge cases with nil values were appearing and throwing errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ class SeedData
       tone_object = db_create_tones(tones, response.question, response.response)
       tone_chat_object = db_create_tone_chat(tone_chat, tone_object.id)
     end
+    puts "Created Tone and Tone Chat Object"
   end
 
   def db_create_tones(tones, question, response)
@@ -47,13 +48,39 @@ class SeedData
                           question_tone: tone_chat[:utterances_tone][0][:tones][0][:score],
                           question_tone_name_two: tone_chat[:utterances_tone][0][:tones][1][:tone_id],
                           question_tone_two: tone_chat[:utterances_tone][0][:tones][1][:score],
-                          response_tone_name: tone_chat[:utterances_tone][1][:tones][0][:tone_id],
-                          response_tone: tone_chat[:utterances_tone][1][:tones][0][:score],
-                          response_tone_name_two: tone_chat[:utterances_tone][1][:tones][1][:tone_id],
-                          response_tone_two:tone_chat[:utterances_tone][1][:tones][1][:score])
+                          response_tone_name: validate_tone_chat_one_name(tone_chat),
+                          response_tone: validate_tone_chat_one(tone_chat),
+                          response_tone_name_two: validate_tone_chat_two_name(tone_chat),
+                          response_tone_two: validate_tone_chat_two(tone_chat))
+  end
+
+  def validate_tone_chat_two_name(tone_chat)
+    if tone_chat[:utterances_tone][1][:tones].length > 1
+      tone_chat[:utterances_tone][1][:tones][1][:tone_id]
+    end
+  end
+
+  def validate_tone_chat_two(tone_chat)
+    if tone_chat[:utterances_tone][1][:tones].length > 1
+      tone_chat[:utterances_tone][1][:tones][1][:score]
+    end
+  end
+
+  def validate_tone_chat_one_name(tone_chat)
+    if tone_chat[:utterances_tone][1][:tones].length > 1
+      tone_chat[:utterances_tone][1][:tones][0][:tone_id]
+    end
+  end
+
+  def validate_tone_chat_one(tone_chat)
+    if tone_chat[:utterances_tone][1][:tones].length > 1
+      tone_chat[:utterances_tone][1][:tones][0][:score]
+    end
   end
 
 end
 
 
-SeedData.new("app/assets/tone_responses/warranty_query_good_outcome_good_tone.csv", 0, 0).load_fixture
+SeedData.new("spec/test_files/accessories_inventory_query_bad_outcome_bad_tone.csv", 2, 0).load_fixture
+SeedData.new("spec/test_files/warranty_query_bad_outcome_bad_tone.csv", 2, 0).load_fixture
+SeedData.new("spec/test_files/warranty_query_good_outcome_good_tone.csv", 0, 0).load_fixture
--- spec/services/calculator_object_spec.rb
+++ spec/services/calculator_object_spec.rb
@@ -1,76 +1,18 @@
 require 'rails_helper'
+require 'tone_fixtures/good_good'
+require 'tone_fixtures/bad_bad'
 
 describe "it can calculate basic functions " do
-  before(:each) do
-    BaseResponse.create!(response_text: "response",
-                          question_text: "question",
-                          disgust: 0.7632,
-                          fear: 0.7632,
-                          joy: 0.7632,
-                          sadness: 0.7632,
-                          anger: 0.7632,
-                          analytical: 0.7632,
-                          confident: 0.7632,
-                          tentative: 0.7632,
-                          openness: 0.7632,
-                          conscientiousness: 0.7632,
-                          extraversion: 0.7632,
-                          agreeableness: 0.7632,
-                          emotional_range: 0.7632)
-    BaseResponse.create!(response_text: "response",
-                          question_text: "question",
-                          disgust: 0.3433,
-                          fear: 0.3433,
-                          joy: 0.3433,
-                          sadness: 0.3433,
-                          anger: 0.3433,
-                          analytical: 0.3433,
-                          confident: 0.3433,
-                          tentative: 0.3433,
-                          openness: 0.3433,
-                          conscientiousness: 0.3433,
-                          extraversion: 0.3433,
-                          agreeableness: 0.3433,
-                          emotional_range: 0.3433)
-    BaseResponse.create!(response_text: "response",
-                          domain: 1,
-                          question_text: "question",
-                          disgust: 0.7632,
-                          fear: 0.7632,
-                          joy: 0.7632,
-                          sadness: 0.7632,
-                          anger: 0.7632,
-                          analytical: 0.7632,
-                          confident: 0.7632,
-                          tentative: 0.7632,
-                          openness: 0.7632,
-                          conscientiousness: 0.7632,
-                          extraversion: 0.7632,
-                          agreeableness: 0.7632,
-                          emotional_range: 0.7632)
-    BaseResponse.create!(response_text: "response",
-                          domain: 1,
-                          question_text: "question",
-                          disgust: 0.99888,
-                          fear: 0.99888,
-                          joy: 0.99888,
-                          sadness: 0.99888,
-                          anger: 0.99888,
-                          analytical: 0.99888,
-                          confident: 0.99888,
-                          tentative: 0.99888,
-                          openness: 0.99888,
-                          conscientiousness: 0.99888,
-                          extraversion: 0.99888,
-                          agreeableness: 0.99888,
-                          emotional_range: 0.99888)
-  end
 
   it "can caluclate average of different domains and specific columns" do
-    output_1 = Calculator.new.average_base_tone(1, "anger")
+    output_1 = Calculator.new.average_base_tone(0, "anger")
     output_2 = Calculator.new.average_base_tone(0, "joy")
 
-    expect(output_1).to eq(0.88104)
-    expect(output_2).to eq(0.55325)
+    expect(output_1).to eq(0.4811)
+    expect(output_2).to eq(0.407766666666667)
+  end
+
+  it "can calculate the geometric mean of domain 1 (private results) and category 0 (good outcome/good tone) tones " do
+    tone_basics = Calculator.new.geometric_mean_base_tones(1, 0)
   end
 end
