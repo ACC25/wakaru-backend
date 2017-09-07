60e9468b0332c802fddac1a9d5fc55739abc0050
Carl Richmond <richmond.carl@outlook.com>
2017-09-06 22:26:51 -0600

    Adds json output, with scores, to a category query from the frontend.

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -22,7 +22,6 @@ class Stat
 
   def find_my_category
     find_percentile_ranks
-    binding.pry
   end
 
   private
@@ -31,9 +30,9 @@ class Stat
     scores.each do |key, value|
       value.each do |k, v|
           scores[key][k] = Response.where(category: k.to_s.split("_")[1]).pluck(:enjoyment_score).extend(DescriptiveStatistics).percentile_rank(db.enjoyment_score) if key == :enjoyment_score
-          scores[key][k] = Response.where(category: k.to_s.split("_")[1]).pluck(:enjoyment_score).extend(DescriptiveStatistics).percentile_rank(db.big_five_score) if key == :big_five_score
+          scores[key][k] = Response.where(category: k.to_s.split("_")[1]).pluck(:big_five_score).extend(DescriptiveStatistics).percentile_rank(db.big_five_score) if key == :big_five_score
           scores[key][k] = Response.where(category: 0).or(Response.where(category: 1)).pluck(:enjoyment_score).extend(DescriptiveStatistics).percentile_rank(db.enjoyment_score) if k == :category_3 && key == :enjoyment_score
-          scores[key][k] = Response.where(category: 0).or(Response.where(category: 1)).pluck(:enjoyment_score).extend(DescriptiveStatistics).percentile_rank(db.big_five_score) if k == :category_3 && key == :big_five_score
+          scores[key][k] = Response.where(category: 0).or(Response.where(category: 1)).pluck(:big_five_score).extend(DescriptiveStatistics).percentile_rank(db.big_five_score) if k == :category_3 && key == :big_five_score
       end
     end
   end
