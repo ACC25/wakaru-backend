77966da9578e7ea9877e5ae0165d4c632fb89ec3
Carl Richmond <richmond.carl@outlook.com>
2017-09-11 19:20:39 -0600

    Adds tests to cover edge cases where users provide invalid login credentials.

--- spec/requests/api/v1/auth/existing_user_can_be_issued_new_token_spec.rb
+++ spec/requests/api/v1/auth/existing_user_can_be_issued_new_token_spec.rb
@@ -18,6 +18,38 @@ describe "user can login with api" do
 
     decoded_token = JWT.decode(reply["token"], ENV['rails_secret_key_base'], true, { :algorithm => 'HS256' })
 
-    expect(decoded_token[0]["user_id"].class).to eq(Fixnum)
+    expect(decoded_token[0]["user_id"]).to eq(company.id)
+  end
+
+  it "sends error if password is invalid" do
+    company = create(:company, name: "Existing User", password: "password")
+    user = {
+      "username": company.name,
+      "password": "wrong password"
+    }
+
+    get '/api/v1/session', :params => user
+
+    expect(response).to be_success
+
+    reply = JSON.parse(response.body)
+
+    expect(reply["error"]).to eq("Invalid login credentials.")
+  end
+
+  it "sends error if password is invalid" do
+    company = create(:company, name: "Existing User", password: "password")
+    user = {
+      "username": "wakaru",
+      "password": company.password
+    }
+
+    get '/api/v1/session', :params => user
+
+    expect(response).to be_success
+
+    reply = JSON.parse(response.body)
+
+    expect(reply["error"]).to eq("Invalid login credentials.")
   end
 end
